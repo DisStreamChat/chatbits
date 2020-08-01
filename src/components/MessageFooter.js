cc13ce3c5f8f4776e327406b62d33b2f7c5c59d1
David Snyder <davidgraygs4@gmail.com>
2020-08-01 19:58:38 -0400

    improved message footer

--- src/components/MessageFooter.js
+++ src/components/MessageFooter.js
@@ -1,55 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import styles from "./MessageFooter.css"
 import TimeIndicator from "./TimeIndicator"
-import DOMPurify from "dompurify";
-import SettingsEthernetIcon from '@material-ui/icons/SettingsEthernet';
-import CodeIcon from '@material-ui/icons/Code';
 
-
-
-const MessageFooter = props => {
-
-    const {showSource, setShowSource} = props
-    const [showSourceButton, setShowSourceButton] = useState(true)
-    const [isOverlay, setIsOverlay] = useState(false)
-
-    useEffect(() => {
-        setShowSourceButton(props.streamerInfo.showSourceButton)
-        setIsOverlay(props.isOverlay)
-    }, [props])
-
+const MessageFooter = React.memo(({sentAt}) => {
     return (
         <div className={styles["message-footer"]}>
-            <div className={`${styles.source} ${showSource && styles["open"]}`}>
-                {showSourceButton && !isOverlay &&
-                    <React.Fragment>
-                        <div className={styles["source-button"]} onClick={() => setShowSource(s => !s)}>{!showSource ? <SettingsEthernetIcon /> : <CodeIcon />}</div>
-                        <p className={styles["source-text"]}>
-                            {(DOMPurify.sanitize(props.body, {
-                                FORBID_ATTR: [
-                                    "style",
-                                    "onerror",
-                                    "onload",
-                                ],
-                                FORBID_TAGS: [
-                                    "table",
-                                    "script",
-                                    "audio",
-                                    "video",
-                                    "style",
-                                    "iframe",
-                                    "textarea",
-                                    "input",
-                                    "form",
-                                ],
-                            }))}
-                        </p>
-                    </React.Fragment>
-                }
+            <div className={`${styles.source}`}>
+                
             </div>
-            <TimeIndicator time={props.sentAt} />
+            <TimeIndicator time={sentAt} />
         </div>
     );
-}
+})
 
 export default MessageFooter;
